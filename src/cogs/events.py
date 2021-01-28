c4067ce6cc68936c438cfe42cec4f52a493ddcb6
bwac <hello@bwac.dev>
2021-01-28 15:44:58 +1100

    added random reminder to vote and donate

--- src/cogs/events.py
+++ src/cogs/events.py
@@ -1,4 +1,5 @@
 import logging
+import random
 
 import discord
 from discord.ext import commands
@@ -50,6 +51,21 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
         await self.refresh_status()
+
+    @commands.Cog.listener()
+    async def on_slash_command(self, ctx):
+        if random.randint(0, 4) == 0:
+            await ctx.channel.send(embeds=[discord.Embed(
+                title="Enjoying redditbot? Vote please!",
+                description="If you voted for me, my creator would really appreciate it",
+                url="https://redditbot.bwac.dev/vote"
+            )])
+        elif random.randint(0, 5) == 0:
+            await ctx.channel.send(embeds=[discord.Embed(
+                title="Enjoying redditbot? If you can, donate please!",
+                description="Donate here to keep it online!",
+                url="https://opencollective.com/redditbot"
+            )])
 
 
 def setup(bot):
