a86cfc3092708d3ad7e86637a3a796984cfb5c5e
bwac <hello@bwac.dev>
2021-02-07 20:39:35 +1100

    changed message

--- src/main.py
+++ src/main.py
@@ -39,7 +39,8 @@ async def _help(ctx):
 async def __help(ctx):
     await help(ctx)
     await ctx.send(
-        "If slash commands aren't working, re-add again with the correct permissions here: https://redditbot.bwac.dev/invite\nIf it keeps happening try again later"
+        "Also, if slash commands aren't working, re-add again with the correct permissions here: "
+        "https://redditbot.bwac.dev/invite\nIf it keeps happening try again later "
     )
 
 
